e6325cf947b5eafdfccfb63dcb2bbef1121ab6a8
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-10-25 13:04:07 +0300

    Add API path for borrowings

--- library_service_project/urls.py
+++ library_service_project/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
     path("api/users/", include("users.urls", namespace="users")),
+    path("api/borrowing/", include("borrowings.urls", namespace="borrowings")),
 ]
