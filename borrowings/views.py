b5f6346e8c6966de709fe64c40dbb7d432bebc83
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-10-27 17:10:15 +0200

    Add `is_active` parameter to filter active or returned borrowings

--- borrowings/views.py
+++ borrowings/views.py
@@ -40,4 +40,11 @@ class BorrowingViewSet(
             if user_id:
                 queryset = queryset.filter(user_id=user_id)
 
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
         return queryset
