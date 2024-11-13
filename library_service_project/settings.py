ef81ef5d966c4d6e56bd629ceac13e3823bca997
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-11-13 13:44:55 +0200

    Add 'drf_spectacular' for OpenAPI schema generation

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -43,11 +43,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
+    "rest_framework.authtoken",
+    "django_q",
     "books",
     "users",
     "borrowings",
-    "rest_framework.authtoken",
-    "django_q",
 ]
 
 MIDDLEWARE = [
